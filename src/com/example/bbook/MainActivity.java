9ce914f61ba3c694a9cc48002b7b318825b45b55
Administrator <Administrator@7A304-0007>
2016-12-19 18:40:48 +0800

    客户端登陆界面

--- src/com/example/bbook/MainActivity.java
+++ src/com/example/bbook/MainActivity.java
@@ -1,34 +1,29 @@
 package com.example.bbook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_firstview);
+		
+		findViewById(R.id.btn_login).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				goLogin();
+			}
+		});
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	
+	void goLogin(){
+		Intent itnt = new Intent(this, LoginActivity.class);
+		startActivity(itnt);
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
