0c3902aef1df3ef208229e509b659ee0cbec2ef0
Administrator <Administrator@7A304-0007>
2016-12-20 10:05:13 +0800

    直接跳登陆(测试用)

--- src/com/example/bbook/LoginActivity.java
+++ src/com/example/bbook/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.bbook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class LoginActivity extends Activity {
 
@@ -9,5 +12,17 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		findViewById(R.id.btn_log).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				goHello();
+			}
+		});
+	}
+	void goHello(){
+		Intent itnt = new Intent(this, HelloWorldActivity.class);
+		startActivity(itnt);
 	}
 }
